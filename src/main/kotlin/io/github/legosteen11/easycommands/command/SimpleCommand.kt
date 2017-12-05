5aa416a2d49471b3c9306d428f20f6a889620bd2
wouter <wouterdoeland@gmail.com>
2017-12-05 17:01:55 +0100

    Added runBlock to SimpleCommand

--- src/main/kotlin/io/github/legosteen11/easycommands/command/SimpleCommand.kt
+++ src/main/kotlin/io/github/legosteen11/easycommands/command/SimpleCommand.kt
@@ -1,7 +1,18 @@
 package io.github.legosteen11.easycommands.command
 
 import io.github.legosteen11.easycommands.CommandHandler
+import io.github.legosteen11.easycommands.user.ICommandSender
 
 abstract class SimpleCommand : ICommand {
     override lateinit var commandHandler: CommandHandler
+
+    /**
+     * Run code in a block that catches and handles exceptions.
+     *
+     * @param commandSender The commandsender
+     * @param block The code block
+     */
+    fun runBlock(commandSender: ICommandSender, block: () -> Unit) {
+        commandHandler.runBlock(commandSender, block)
+    }
 }
